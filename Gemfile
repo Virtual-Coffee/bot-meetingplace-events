049c2df3748fb8580145fdb1136f7ee5ec9f7d67
Mike Rogers <me@mikerogers.io>
2021-01-18 11:03:22 +0000

    Switching to standardrb for Ruby Linting (#35)
    * Switching Rubocop out for Standard
    
    * standardrb --fix

--- Gemfile
+++ Gemfile
@@ -1,18 +1,18 @@
-source 'https://rubygems.org'
-ruby File.read('.ruby-version').chomp
+source "https://rubygems.org"
+ruby File.read(".ruby-version").chomp
 
-gem 'activesupport'
-gem 'dotenv'
-gem 'dotiw'
-gem 'rake'
-gem 'slack-ruby-bot'
-gem 'zeitwerk'
+gem "activesupport"
+gem "dotenv"
+gem "dotiw"
+gem "rake"
+gem "slack-ruby-bot"
+gem "zeitwerk"
 
 group :test do
-  gem 'rack-test'
-  gem 'rspec'
-  gem 'simplecov', require: false
-  gem 'timecop'
-  gem 'vcr'
-  gem 'webmock'
+  gem "rack-test"
+  gem "rspec"
+  gem "simplecov", require: false
+  gem "timecop"
+  gem "vcr"
+  gem "webmock"
 end
--- spec/meeting_place/events_spec.rb
+++ spec/meeting_place/events_spec.rb
@@ -1,14 +1,14 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe MeetingPlace::Events do
-  let(:instance) { described_class.new('virtual-coffee') }
-  let(:events_json) { File.read('spec/fixtures/files/meetingplace.io/events.json') }
+  let(:instance) { described_class.new("virtual-coffee") }
+  let(:events_json) { File.read("spec/fixtures/files/meetingplace.io/events.json") }
   let!(:events_endpoint) do
-    stub_request(:get, 'https://meetingplace.io/api/v1/group/virtual-coffee/events.json')
+    stub_request(:get, "https://meetingplace.io/api/v1/group/virtual-coffee/events.json")
       .and_return(status: 200, body: events_json)
   end
 
-  describe '#call' do
+  describe "#call" do
     subject { instance.call }
 
     it do
